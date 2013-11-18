e69cb87248fa3bca790a7bca687f3b688ee942e6
HoldenCaulfieldICL <ad6813@imperial.ac.uk>
2013-11-18 22:35:33 +0000

    finished

--- lifts.c
+++ lifts.c
@@ -1,21 +1,6 @@
-/*I assume that Central takes all decisions, but that it does not execute a function 
-that operates the hardware if that hardware is controlled by a CPU on a floor or a carriage.
-On the other hand, a non-central process does not ever execute a function that operates
-hardware unless it has received a message from Central telling it to do so.
-The result is that Central commands everything but does not manipulate any hardware, like a 
-conductor and his orchestra.*/
-
-
-/*CHECK PRIORITISATION!!!*/
-
-/*don't assume shaft number is known, if shaft number is sent as info, received_msg should be
-overloaded with a second msg_number. simply have another if statement for this voerloaded 
-version. notice OPEN_DOORS has no msg_number so need underloaded as well*/
-
 process Central() 
 {
   int floors_receiving_elevator[number_of_floors] = 0;
-  /*initialised as an array of zeroes*/
 
   for (;;)
     {
@@ -23,183 +8,133 @@ process Central()
 	{
 	  switch(content) 
 	    {
-	    case 'CARRIAGE_AT_FLOOR':
-	      /*high priority: need to open doors quickly. assume shaft number is also known*/
-	      send_msg(Floor[msg_number][shaft_nb], OPEN_DOORS);
+	    case 'TIME_OUT':
 	      break;
 
 	    case 'REQUEST_BUTTON_PRESSED':
-	      /*this message comes from Floor to say its button has been pressed*/
-	      switch (floors_receiving_elevator[msg_number]) {
-	      case 1:
-		/*do nothing: there is already an elevator coming to that floor, 
-		  passengers may have to move to another door on same floor, I 
-		  assume they can see all doors*/
-		break;
-
-	      case 0:
-		/*add floor to destinations of nearest elevator and update floor array*/
-		send_msg(Elevator[closest], ADD_FLOOR_TO_DEST, floor_nb);
+	      /*message comes from Floor to say a lift has been called to it*/
+	      if (floors_receiving_elevator[msg_number] == 1);
+	      /*do nothing: there is already a lift coming to that floor, 
+		passengers may have to move to another door on same floor, I 
+		assume they can easily do this*/
+	      else {
+		/*pass message on to nearest Elevator and update floor array*/
+		send_msg(Elevator[closest], REQUEST_BUTTON_PRESSED, floor_nb);
 		floors_receiving_elevator[msg_number] = 1;
-	      }
-	      break;
+	      } break;
 
 	    case 'FLOOR_BUTTON_PRESSED' :
-	      /*this comes from Elevator to say that one of its passengers has asked
-		to go to a floor not on the elevator's destinations*/
-	      floor_receiving_elevator[floor_nb] = 1;
+	      /*sender==Elevator; Central needs to know destinations of each 
+		Elevator to be able to take the right decision in 
+		case 'REQUEST_BUTTON_PRESSED'*/
+	      floor_receiving_elevator[floor_nb] = 1; break;
 	    }
 	}
     }
 }
-
 
 process Elevator() 
 {
-  /*initialised as an array of zeroes*/
-  int destinations[number_of_floors] = 0;
+  /*Elevator hardware sends signals via 'pressed' and 'number'.
+    Elevator has access to 'reset'. */
+  struct Floor_button {
+    bool pressed = false;
+    int number;
+    void reset();
+  } floor_button;
 
-  /*queue of integers representing floors to go to*/
-  Queue itinerary;
+  /*Hardware sends signals via 'open_door' and 'close_door'*/
+  bool open_door_button = false;
+  bool close_door_button = false;
 
-  void compute_optimal(Queue &itinerary);
+  /*Controlled by Elevator*/
+  struct Itinerary {
+    Queue list;
+    void add_destination(int floor_number);
+    void compute_optimal();
+    void go_to_next_destination();
+  } itinerary;
 
-  /*dequeues itinerary, goes to floor corresponding to dequeued value*/
-  void go_to_next_destination(Queue &itinerary);
+  /*Controlled by Elevator: stops lift in its track*/
+  void halt();
 
   for (;;)
     {
-      if (received_msg(sender, content, msg_number) == true)
-	{
-	  switch(content) 
-	    {
-	    case 'ADD_FLOOR_TO_DEST':
-	      /*this message comes from Central to say to add a floor to list of 
-		destinations*/
-	      destinations[msg_number] = 1;
-	      compute_optimal(itinerary);
-	      break;
+      if (received_msg(Elevator, 'CARRIAGE_AT_FLOOR', msg_number) == true) {
+	/*interrupt informs Elevator it has arrived at a destination. highest priority:
+	  lift needs to halt in its track at exactly the right moment, and message Floor*/
+	halt();
+	send_msg(Floor, OPEN_DOOR, msg_number);
+      }
 
-	    case 'FLOOR_BUTTON_PRESSED' :
-	      /*this message comes from the carriage to say that a local passenger
-		would like to add a floor to destinations. I suppose that it is only
-		ever pressed when corresponding floor is not yet a destination*/
-	      destinations[msg_number] = 1;
-	      compute_optimal(itinerary);
-	      send_msg(Central, FLOOR_BUTTON_PRESSED, msg_number);
-	    }
-	}
-      go_to_next_destination();
+      if (open_door_button)
+	/*hardware signals 'open door' button pressed in carriage. assuming button 
+	  only ever pressed in correct situation, no need for Central to take a decision: 
+	  message sent directly to Floor. high priority, could be urgent.*/
+	send_msg(Floor, OPEN_DOOR, msg_number);
+
+      if (received_msg(Elevator, 'REQUEST_BUTTON_PRESSED', msg_number) == true) {
+	/*sender==Central, but message really comes from a Floor to tell Elevator to
+	  come to it. floor number is added to destinations.*/
+	itinerary.add_destination(msg_number);
+	itinerary.compute_optimal(); break;
+      }
+      if (floor_button.pressed) {
+	/*hardware signals that button pressed in carriage. Passengers cannot take
+	  another lift, so no need for Central to take a decision: Elevator adds floor
+	  to destinations; but important info message sent for Central to update.*/
+	itinerary.add_destination(floor_button.number);
+	itinerary.compute_optimal();
+	floor_button.reset();
+	send_msg(Central, FLOOR_BUTTON_PRESSED, floor_button.number);
+      }
+      if (close_door_button)
+	/*lowest priority: go to case 'CLOSE_DOOR' in Floor to find out why*/
+	send_msg(Floor, CLOSE_DOOR, msg_number);
+
+      itinerary.go_to_next_destination();
     }
 }
-
 
 process Floor()
 {
   /*tells CPU on floor to open doors*/
-  void open_doors();
+  void open_door();
 
+  /*initiates time-out service which will cause a message to be sent back to 
+    Floor after a pre-set amount of time*/
   void initiate_time_out();
+
+  /*Controlled by time-out service; set to true by initiate_time_out()*/
+  bool time_out = false;
 
   for(;;)
     {
       if (received_msg(sender, content, msg_number) == true)
 	{
-	  switch(content) 
-	    {
-	    case 'OPEN_DOORS':
-	      open_doors();
-	      intitiate_time_out();
-	      break;
+	  switch(content) {
+	  case 'OPEN_DOOR':
+	    /*sender==Elevator; assuming button only pressed in correct situation,
+	      doors are always opened, and time-out service is initiated.*/
+	    open_door();
+	    initiate_time_out(); break;
 
-	    case 'REQUEST_BUTTON_PRESSED':
-	      /*send message to Central with floor number as msg_number*/
-	      send_msg(Central, REQUEST_BUTTON_PRESSED, msg_number);
-	      break;
+	  case 'CARRIAGE_AT_FLOOR':
+	    /*sender==Elevator; it has arrived at floor, needs door to open*/
+	    open_door();
+	    intitiate_time_out(); break;
 
-	    case '':
+	  case 'REQUEST_BUTTON_PRESSED':
+	    /*pass message along to Central with floor number as msg_number. 
+	      low priority: unlikely that floor will be skipped within time delay.*/
+	    send_msg(Central, REQUEST_BUTTON_PRESSED, msg_number); break;
 
-	    }
+	  case 'CLOSE_DOOR':
+	    /*do nothing: if door already closed, no need to close it again.
+	      if door open and not TIME_OUT, we need to wait for 'TIME_OUT'.
+	      if door open and TIME_OUT, then we must be at case 'TIME_OUT', not here*/
+	    break;
+	  }
 	}
     }
 }
-
-
-
-
-
-
-
-
-
-
- 
-/* 	{ */
-/* 	  if (floors_receiving_elevator[i] == 0)  */
-/* 	    { */
-/* 	      /\*send_closest_elevator_to_floor_i; dont't need this,  */
-/* 		process can only message, not actually direct,  */
-/* 		need elevator process to do that*\/ */
-/* 	      floors_receiving_elevator[i] = 1; */
-/* 	      Central_messages_closest_Elevator("add floor ", i, " to your destinations"); */
-/* 	      Central_messages_Process_i("elevator on its way to you"); */
-/* 	    } */
-/* 	  if (floors_receiving_elevator[i] == 1)  */
-/* 	    { */
-/* 	      Central_messages_Floor_[i][all]("elevator on its way to you"); */
-/* 	    } */
-/* 	} */
-
-/*       if(received_message_from_Elevator_i) */
-/* 	{ */
-/* 	  if message_is(CALL_LIFT, floor n); */
-/* 	  { */
-/* 	    floors_receiving_elevator[n] = 1; */
-/* 	    elevators_going_to_floors[i][n] = 1; */
-/* 	    Central_messages_Elevator_i("add floor", n, " to your destinations"); */
-/* 	  } */
-/* 	  if message_is(OPEN_DOOR, floor n); */
-/* 	  { */
-/* 	    Central_messages_Floor_[n][i]("open your doors"); */
-/* 	  } */
-/* 	  if message_is(CLOSE_DOOR, floor n); */
-/* 	  { */
-/* 	    Central_messages_Floor_[n][i]("close your doors");		 */
-/* 	  } */
-/* 	} */
-/*     } */
-/* } */
-
-
-/* process Elevator[i]()  */
-/* { */
-/*   for (;;) */
-/*     { */
-/*       for (;no_button_pressed;) */
-/* 	{ */
-/* 	} */
-
-/*       if (FLOOR_BUTTON_PRESSED == n) */
-/* 	{ */
-/* 	  message_Central(FLOOR_BUTTON_PRESSED, floor_number); */
-/* 	} */
-/*     } */
-/* } */
-
-/* if ( */
-
-/*     } */
-
-/*   process Floor()  */
-/* { */
-/*   for(;;)  */
-/*     { */
-/*       for (; CALL_LIFT == 0;)  */
-/* 	{ */
-/* 	} */
-/*       Floor_messages_Central("floor number ", floor_number, " has been called"); */
-	
-/*     } */
-
-/* } */
-
